9a4ab17b92e24272da30ea1d516e6d00edec305e
Ben Parisot <me@benparisot.com>
2020-01-25 10:02:00 -0800

    get traffic light working

---
+++ src/components/TrafficLight.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import './TrafficLight.css';
+
+export default function TrafficLight() {
+    const [colors, setColors] = useState({
+        red: 'light red',
+        yellow: 'light',
+        green: 'light'
+    });
+
+    const updateColor = () => {
+        if(colors.red === 'light red') {
+            setColors({
+                red: 'light',
+                yellow: 'light yellow',
+                green: 'light'
+            })
+        }
+        if (colors.yellow === 'light yellow') {
+            setColors({
+                red: 'light',
+                yellow: 'light',
+                green: 'light green'
+            })
+        }
+        if (colors.green === 'light green') {
+            setColors({
+                red: 'light red',
+                yellow: 'light',
+                green: 'light'
+            })
+        }
+    }
+    const changeColor = () => setTimeout(updateColor, 2000);
+
+    changeColor();
+
+    return (
+        <div className="light-container">
+            <div className={colors.red}></div>
+            <div className={colors.yellow}></div>
+            <div className={colors.green}></div>
+        </div>
+    )
+}
